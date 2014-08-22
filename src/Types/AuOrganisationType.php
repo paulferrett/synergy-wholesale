578b8100e2df965eced9df9edfd252d953aab315
Simon Hampel <simon@hampelgroup.com>
2014-08-23 09:36:12 +1000

    new type - AuOrganisationType

---
+++ src/Types/AuOrganisationType.php
@@ -0,0 +1,61 @@
+<?php  namespace SynergyWholesale\Types; 
+
+use SynergyWholesale\Exception\UnknownOrganisationTypeException;
+
+class AuOrganisationType
+{
+	private $organisationType;
+
+	private static $organisationTypes = array(
+		'Charity',
+		'Citizen/Resident',
+		'Club',
+		'Commercial Statutory Body',
+		'Company',
+		'Incorporated Association',
+		'Industry Body',
+		'Non-profit Organisation',
+		'Other',
+		'Partnership',
+		'Pending TM Owner',
+		'Political Party',
+		'Registered Business',
+		'Religious/Church Group',
+		'Sole Trader',
+		'Trade Union',
+		'Trademark'
+	);
+
+	function __construct($organisationType)
+	{
+		if (!in_array($organisationType, static::$organisationTypes))
+		{
+			throw new UnknownOrganisationTypeException("Unknown organisation type [{$organisationType}]");
+		}
+
+		$this->organisationType = $organisationType;
+	}
+
+	public function getOrganisationType()
+	{
+		return $this->organisationType;
+	}
+
+	public function isRegisteredBusiness()
+	{
+		return $this->organisationType == 'Registered Business';
+	}
+
+	public function isCompany()
+	{
+		return $this->organisationType == 'Company';
+	}
+
+	public function __toString()
+	{
+		return $this->getOrganisationType();
+	}
+}
+
+?>
+ 
---
+++ tests/Types/AuOrganisationTypeTest.php
@@ -0,0 +1,28 @@
+<?php  namespace SynergyWholesale\Types;
+
+class AuOrganisationTypeTest extends \PHPUnit_Framework_TestCase
+{
+	public function testBadOrganisation()
+	{
+		$this->setExpectedException('SynergyWholesale\Exception\UnknownOrganisationTypeException', 'Unknown organisation type [foo]');
+
+		$org = new AuOrganisationType('foo');
+	}
+
+	public function testOrganisation()
+	{
+		$org = new AuOrganisationType('Company');
+		$this->assertEquals('Company', $org->getOrganisationType());
+		$this->assertEquals('Company', strval($org));
+		$this->assertTrue($org->isCompany());
+		$this->assertFalse($org->isRegisteredBusiness());
+
+		$org = new AuOrganisationType('Registered Business');
+		$this->assertEquals('Registered Business', $org->getOrganisationType());
+		$this->assertTrue($org->isRegisteredBusiness());
+		$this->assertFalse($org->isCompany());
+	}
+}
+
+?>
+ 
