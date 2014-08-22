ab03b73cb6325e2de716fd69db447f80ee4d5167
Simon Hampel <simon@hampelgroup.com>
2014-08-23 09:37:07 +1000

    new type - AuRegistrant

---
+++ src/Types/AuRegistrant.php
@@ -0,0 +1,131 @@
+<?php  namespace SynergyWholesale\Types; 
+
+use SynergyWholesale\Exception\InvalidArgumentException;
+
+class AuRegistrant
+{
+	private $registrantName;
+
+	private $registrantId;
+
+	/** @var \SynergyWholesale\Types\AuIdType 'ABN', 'ACN', 'NSW BN', etc */
+	private $registrantIdType;
+
+	/** @var \SynergyWholesale\Types\AuOrganisationType Charity, Company, Incorporated Association, Registered Business, etc */
+	private $eligibilityType;
+
+	/** @var entityName */
+	private $eligibilityName;
+
+	/** @var ACN number, ABN number, BN number, etc  */
+	private $eligibilityId;
+
+	/** @var \SynergyWholesale\Types\AuIdType 'ABN', 'ACN', 'NSW BN', etc */
+	private $eligibilityIdType;
+
+	function __construct(
+		$registrantName,
+		$registrantId,
+		AuOrganisationType $eligibilityType,
+		AuIdType $registrantIdType = null,
+		$eligibilityName = null,
+		$eligibilityId = null,
+		AuIdType $eligibilityIdType = null
+	)
+	{
+		if (empty($registrantName))
+		{
+			throw new InvalidArgumentException("Missing argument: registrantName");
+		}
+
+		if (empty($registrantId) AND $eligibilityType->isCompany())
+		{
+			throw new InvalidArgumentException("Missing argument for Company registrant: registrantId");
+		}
+
+		$this->registrantName = $registrantName;
+		$this->registrantId = $registrantId;
+		$this->eligibilityType = $eligibilityType;
+		$this->registrantIdType = $registrantIdType;
+		$this->eligibilityName = $eligibilityName;
+		$this->eligibilityId = $eligibilityId;
+		$this->eligibilityIdType = $eligibilityIdType;
+	}
+
+	public static function newFromAuBusinessRegistration(AuBusinessRegistration $registration, $registrantName = "")
+	{
+		$eligibilityType = $registration->getOrganisationType();
+
+		if ($eligibilityType->isRegisteredBusiness())
+		{
+			if (empty($registrantName))
+			{
+				throw new InvalidArgumentException("registrantName parameter is required for a Registered Business");
+			}
+
+			$registrantId = null;
+			$registrantIdType = null;
+			$eligibilityName = $registration->getEntityName();
+			$eligibilityId = $registration->getRegistrationNumber();
+			$eligibilityIdType = AuIdType::newFromState($registration->getState());
+		}
+		elseif ($eligibilityType->isCompany())
+		{
+			$registrantName = $registration->getEntityName();
+			$registrantId = $registration->getRegistrationNumber();
+			$registrantIdType = strlen($registrantId) < 11 ? AuIdType::newAcn() : AuIdType::newAbn();
+			$eligibilityName = null;
+			$eligibilityId = null;
+			$eligibilityIdType = null;
+		}
+		else
+		{
+			$registrantName = $registration->getEntityName();
+			$registrantId = $registration->getRegistrationNumber();
+			$registrantIdType = AuIdType::newOther();
+			$eligibilityName = $registration->getLegalName();
+			$eligibilityId = null;
+			$eligibilityIdType = null;
+		}
+
+		return new static($registrantName, $registrantId, $eligibilityType, $registrantIdType, $eligibilityName, $eligibilityId, $eligibilityIdType);
+	}
+
+	public function getRegistrantName()
+	{
+		return $this->registrantName;
+	}
+
+	public function getRegistrantId()
+	{
+		return $this->registrantId;
+	}
+
+	public function getEligibilityType()
+	{
+		return $this->eligibilityType;
+	}
+
+	public function getRegistrantIdType()
+	{
+		return $this->registrantIdType;
+	}
+
+	public function getEligibilityName()
+	{
+		return $this->eligibilityName;
+	}
+
+	public function getEligibilityId()
+	{
+		return $this->eligibilityId;
+	}
+
+	public function getEligibilityIdType()
+	{
+		return $this->eligibilityIdType;
+	}
+}
+
+?>
+ 
---
+++ tests/Types/AuRegistrantTest.php
@@ -0,0 +1,103 @@
+<?php  namespace SynergyWholesale\Types;
+
+class AuRegistrantTest extends \PHPUnit_Framework_TestCase
+{
+	public function testMissingRegistrantName()
+	{
+		$this->setExpectedException('SynergyWholesale\Exception\InvalidArgumentException', 'Missing argument: registrantName');
+
+		$reg = new AuRegistrant("", "", new AuOrganisationType('Company'));
+	}
+
+	public function testMissingRegistrantId()
+	{
+		$this->setExpectedException('SynergyWholesale\Exception\InvalidArgumentException', 'Missing argument for Company registrant: registrantId');
+
+		$reg = new AuRegistrant("foo", "", new AuOrganisationType('Company'));
+	}
+
+	public function testNewMissingRegistrantName()
+	{
+		$this->setExpectedException('SynergyWholesale\Exception\InvalidArgumentException', 'registrantName parameter is required for a Registered Business');
+
+		$br = new AuBusinessRegistration(
+			"B111",
+			"Registered",
+			"",
+			"foo",
+			"",
+			"",
+			new AuOrganisationType('Registered Business'),
+			new AuState('NSW')
+		);
+
+		$reg = AuRegistrant::newFromAuBusinessRegistration($br);
+	}
+
+	public function testRegistrant()
+	{
+		$reg = new AuRegistrant("foo", "bar", new AuOrganisationType('Company'));
+		$this->assertEquals('foo', $reg->getRegistrantName());
+		$this->assertEquals('bar', $reg->getRegistrantId());
+		$this->assertInstanceOf('SynergyWholesale\Types\AuOrganisationType', $reg->getEligibilityType());
+		$this->assertEquals('Company', $reg->getEligibilityType()->getOrganisationType());
+	}
+
+	public function testNew()
+	{
+		$br = new AuBusinessRegistration(
+			"111111111",
+			"Registered",
+			"",
+			"foo",
+			"",
+			"",
+			new AuOrganisationType('Company')
+		);
+
+		$reg = AuRegistrant::newFromAuBusinessRegistration($br);
+		$this->assertEquals('foo', $reg->getRegistrantName());
+		$this->assertEquals('111111111', $reg->getRegistrantId());
+		$this->assertEquals('ACN', $reg->getRegistrantIdType()->getIdType());
+		$this->assertEquals('Company', $reg->getEligibilityType()->getOrganisationType());
+		$this->assertNull($reg->getEligibilityName());
+		$this->assertNull($reg->getEligibilityId());
+		$this->assertNull($reg->getEligibilityIdType());
+
+		$br = new AuBusinessRegistration(
+			"11111111111",
+			"Registered",
+			"",
+			"foo",
+			"",
+			"",
+			new AuOrganisationType('Company')
+		);
+
+		$reg = AuRegistrant::newFromAuBusinessRegistration($br);
+		$this->assertEquals('ABN', $reg->getRegistrantIdType()->getIdType());
+
+		$br = new AuBusinessRegistration(
+			"B111",
+			"Registered",
+			"",
+			"foo",
+			"",
+			"",
+			new AuOrganisationType('Registered Business'),
+			new AuState('NSW')
+		);
+
+		$reg = AuRegistrant::newFromAuBusinessRegistration($br, 'bar');
+		$this->assertEquals('bar', $reg->getRegistrantName());
+		$this->assertNull($reg->getRegistrantId());
+		$this->assertNull($reg->getRegistrantIdType());
+		$this->assertEquals('Registered Business', $reg->getEligibilityType()->getOrganisationType());
+		$this->assertEquals('foo', $reg->getEligibilityName());
+		$this->assertEquals('B111', $reg->getEligibilityId());
+		$this->assertEquals('NSW BN', $reg->getEligibilityIdType()->getIdType());
+
+	}
+}
+
+?>
