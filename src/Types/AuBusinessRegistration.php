cc7fb792adafa0499a24fd37e3561a6e64ca4c78
Simon Hampel <simon@hampelgroup.com>
2014-08-23 09:37:35 +1000

    new type - AuBusinessRegistration

---
+++ src/Types/AuBusinessRegistration.php
@@ -0,0 +1,106 @@
+<?php  namespace SynergyWholesale\Types; 
+
+class AuBusinessRegistration
+{
+	private $registrationNumber;
+	private $entityStatus;
+	private $asicNumber;
+	private $entityName;
+	private $tradingName;
+	private $legalName;
+	private $organisationType;
+	private $state;
+	private $postcode;
+
+	function __construct(
+		$registrationNumber,
+		$entityStatus,
+		$asicNumber,
+		$entityName,
+		$tradingName,
+		$legalName,
+		AuOrganisationType $organisationType,
+		AuState $state = null,
+		AuPostCode $postcode = null
+	)
+	{
+		$this->registrationNumber = $registrationNumber;
+		$this->entityStatus = $entityStatus;
+		$this->asicNumber = $asicNumber;
+		$this->entityName = $entityName;
+		$this->tradingName = $tradingName;
+		$this->legalName = $legalName;
+		$this->organisationType = $organisationType;
+		$this->state = $state;
+		$this->postcode = $postcode;
+	}
+
+	public function getRegistrationNumber()
+	{
+		return $this->registrationNumber;
+	}
+
+	public function getEntityStatus()
+	{
+		return $this->entityStatus;
+	}
+
+	public function getAsicNumber()
+	{
+		return $this->asicNumber;
+	}
+
+	public function getEntityName()
+	{
+		return $this->entityName;
+	}
+
+	public function getTradingName()
+	{
+		return $this->tradingName;
+	}
+
+	public function getLegalName()
+	{
+		return $this->legalName;
+	}
+
+	public function getOrganisationType()
+	{
+		return $this->organisationType;
+	}
+
+	public function getOrganisationTypeName()
+	{
+		return $this->organisationType->getOrganisationType();
+	}
+
+	public function getState()
+	{
+		return $this->state;
+	}
+
+	public function getStateName()
+	{
+		if (isset($this->state))
+		{
+			return $this->state->getState();
+		}
+	}
+
+	public function getPostcode()
+	{
+		return $this->postcode;
+	}
+
+	public function getPostcodeString()
+	{
+		if (isset($this->postcode))
+		{
+			return $this->postcode->getPostcode();
+		}
+	}
+}
+
+?>
+ 
---
+++ tests/Types/AuBusinessRegistrationTest.php
@@ -0,0 +1,39 @@
+<?php  namespace SynergyWholesale\Types;
+
+class AuBusinessRegistrationTest extends \PHPUnit_Framework_TestCase {
+
+	public function testAuBusinessRegistration()
+	{
+		$br = new AuBusinessRegistration(
+			"11111111111",
+			"Registered",
+			"111111111",
+			"foo",
+			"bar",
+			"foobar",
+			new AuOrganisationType('Company'),
+			new AuState('NSW'),
+			new AuPostCode('2000')
+		);
+
+		$this->assertEquals('11111111111', $br->getRegistrationNumber());
+		$this->assertEquals('Registered', $br->getEntityStatus());
+		$this->assertEquals('111111111', $br->getAsicNumber());
+		$this->assertEquals('foo', $br->getEntityName());
+		$this->assertEquals('bar', $br->getTradingName());
+		$this->assertEquals('foobar', $br->getLegalName());
+		$this->assertInstanceOf('SynergyWholesale\Types\AuOrganisationType', $br->getOrganisationType());
+		$this->assertEquals('Company', $br->getOrganisationType()->getOrganisationType());
+		$this->assertEquals('Company', $br->getOrganisationTypeName());
+		$this->assertInstanceOf('SynergyWholesale\Types\AuState', $br->getState());
+		$this->assertEquals('NSW', $br->getState()->getState());
+		$this->assertEquals('NSW', $br->getStateName());
+		$this->assertInstanceOf('SynergyWholesale\Types\AuPostCode', $br->getPostcode());
+		$this->assertEquals('2000', $br->getPostcode()->getPostcode());
+		$this->assertEquals('2000', $br->getPostcodeString());
+	}
+
+}
+
+?>
+ 
