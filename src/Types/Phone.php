f356ed371b80b718bc8ecf045a6cd1bf9b5a369d
Simon Hampel <simon@hampelgroup.com>
2014-08-23 09:34:02 +1000

    new type - Phone

---
+++ src/Types/Phone.php
@@ -0,0 +1,33 @@
+<?php  namespace SynergyWholesale\Types; 
+
+use SynergyWholesale\Exception\InvalidArgumentException;
+
+class Phone
+{
+	private $phone;
+
+	function __construct($phone)
+	{
+		// strip all spaces
+		$phone = str_replace(' ', '', $phone);
+
+		if (!preg_match('/^\+[0-9]{1,2}\.[0-9]{6,9}$/', $phone))
+		{
+			throw new InvalidArgumentException("Invalid number [{$phone}] - must be in the format +99.999999999");
+		}
+		$this->phone = $phone;
+	}
+
+	public function getPhone()
+	{
+		return $this->phone;
+	}
+
+	public function __toString()
+	{
+		return $this->getPhone();
+	}
+}
+
+?>
+ 
---
+++ tests/Types/PhoneTest.php
@@ -0,0 +1,41 @@
+<?php  namespace SynergyWholesale\Types;
+
+class PhoneTest extends \PHPUnit_Framework_TestCase {
+
+	public function testBadPhone1()
+	{
+		$this->setExpectedException('SynergyWholesale\Exception\InvalidArgumentException', 'Invalid number [] - must be in the format +99.999999999');
+
+		$phone = new Phone('');
+	}
+
+	public function testBadPhone2()
+	{
+		$this->setExpectedException('SynergyWholesale\Exception\InvalidArgumentException', 'Invalid number [+0.0] - must be in the format +99.999999999');
+
+		$phone = new Phone('+0.0');
+	}
+
+	public function testBadPhone3()
+	{
+		$this->setExpectedException('SynergyWholesale\Exception\InvalidArgumentException', 'Invalid number [+AA.AAAAAAAAA] - must be in the format +99.999999999');
+
+		$phone = new Phone('+AA.AAAAAAAAA');
+	}
+
+	public function testPhone()
+	{
+		$phone = new Phone('+00.000000000');
+		$this->assertEquals('+00.000000000', $phone->getPhone());
+
+		$phone = new Phone('+0.000000');
+		$this->assertEquals('+00.000000000', $phone->getPhone());
+
+		// test space stripping
+		$phone = new Phone('+00.0 0000 0000');
+		$this->assertEquals('+00.000000000', $phone->getPhone());
+	}
+}
+
+?>
+ 
