fcfb7efa1a7e76f995798d393eab7baf9601b7c3
Simon Hampel <simon@hampelgroup.com>
2014-08-23 09:17:19 +1000

    new type - Country

---
+++ src/Types/Country.php
@@ -0,0 +1,44 @@
+<?php  namespace SynergyWholesale\Types; 
+
+use SynergyWholesale\Exception\UnknownCountryException;
+
+class Country
+{
+	private static $isocodes;
+
+	private $countryCode;
+
+	public function __construct($countryCode)
+	{
+		$countryCode = strtoupper($countryCode);
+
+		if (!isset(static::$isocodes))
+		{
+			static::$isocodes = require __DIR__ . '/isocodes.php';
+		}
+
+		if (!array_key_exists($countryCode, static::$isocodes))
+		{
+			throw new UnknownCountryException("Unknown country [{$countryCode}]");
+		}
+		$this->countryCode = $countryCode;
+	}
+
+	public function getCountryCode()
+	{
+		return $this->countryCode;
+	}
+
+	public function getCountryName()
+	{
+		return self::$isocodes[$this->countryCode];
+	}
+
+	public function __toString()
+	{
+		return $this->getCountryName();
+	}
+}
+
+?>
+ 
---
+++ tests/Types/CountryTest.php
@@ -0,0 +1,38 @@
+<?php  namespace SynergyWholesale\Types; 
+
+class CountryTest extends \PHPUnit_Framework_TestCase {
+
+	public function testBadCountry1()
+	{
+		$this->setExpectedException('SynergyWholesale\Exception\UnknownCountryException', 'Unknown country []');
+
+		$country = new Country('');
+	}
+
+	public function testBadCountry2()
+	{
+		$this->setExpectedException('SynergyWholesale\Exception\UnknownCountryException', 'Unknown country [AA]');
+
+		$country = new Country('AA');
+	}
+
+	public function testBadCountry3()
+	{
+		$this->setExpectedException('SynergyWholesale\Exception\UnknownCountryException', 'Unknown country [AUS]');
+
+		$country = new Country('AUS');
+	}
+
+	public function testCountry()
+	{
+		$country = new Country('au');
+		$this->assertEquals('AU', $country->getCountryCode());
+
+		$country = new Country('AU');
+		$this->assertEquals('AU', $country->getCountryCode());
+		$this->assertEquals('Australia', $country->getCountryName());
+	}
+}
+
+?>
+ 
