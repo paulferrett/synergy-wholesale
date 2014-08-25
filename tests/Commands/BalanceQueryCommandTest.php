273d0d1086846a90ac02563d1a59f71d78f8c106
Simon Hampel <simon@hampelgroup.com>
2014-08-25 16:19:39 +1000

    updates for BalanceQueryCommandTest unit tests

--- tests/Commands/BalanceQueryCommandTest.php
+++ tests/Commands/BalanceQueryCommandTest.php
@@ -4,7 +4,7 @@ use stdClass;
 
 class BalanceQueryCommandTest extends \PHPUnit_Framework_TestCase
 {
-	public function testBuild()
+	public function testGetRequestData()
 	{
 		$command = new BalanceQueryCommand();
 		$build = $command->getRequestData();
@@ -12,65 +12,6 @@ class BalanceQueryCommandTest extends \PHPUnit_Framework_TestCase
 		$this->assertTrue(is_array($build));
 		$this->assertTrue(empty($build));
 	}
-/*
-	public function testParseResponseMissingStatus()
-	{
-		$response = new stdClass();
-
-		$this->setExpectedException('SynergyWholesale\Exception\ResponseErrorException', 'Expected property [status] missing from response data');
-
-		$command = new BalanceQueryCommand();
-		$command->parseResponse($response);
-	}
-
-	public function testParseResponseMissingBalance()
-	{
-		$response = new stdClass();
-		$response->status = "foo";
-
-		$this->setExpectedException('SynergyWholesale\Exception\ResponseErrorException', 'Expected property [balance] missing from response data');
-
-		$command = new BalanceQueryCommand();
-		$command->parseResponse($response);
-	}
-
-	public function testParseResponseNoErrorMessage()
-	{
-		$response = new stdClass();
-		$response->status = "foo";
-		$response->balance = "bar";
-
-		$this->setExpectedException('SynergyWholesale\Exception\ResponseErrorException', 'Unknown error');
-
-		$command = new BalanceQueryCommand();
-		$command->parseResponse($response);
-	}
-
-	public function testParseResponseWithErrorMessage()
-	{
-		$response = new stdClass();
-		$response->status = "foo";
-		$response->balance = "bar";
-		$response->errorMessage = "baz";
-
-		$this->setExpectedException('SynergyWholesale\Exception\ResponseErrorException', 'baz');
-
-		$command = new BalanceQueryCommand();
-		$command->parseResponse($response);
-	}
-
-	public function testParseResponse()
-	{
-		$response = new stdClass();
-		$response->status = "OK";
-		$response->balance = "bar";
-
-		$command = new BalanceQueryCommand();
-		$data = $command->parseResponse($response);
-
-		$this->assertEquals('bar', $data);
-	}
-*/
 }
 
 ?>
