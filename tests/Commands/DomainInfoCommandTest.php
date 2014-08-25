c197d2c31981ef487b3f5d82ca87051a04d30b13
Simon Hampel <simon@hampelgroup.com>
2014-08-25 16:20:14 +1000

    unit tests for DomainInfoCommand

---
+++ tests/Commands/DomainInfoCommandTest.php
@@ -0,0 +1,19 @@
+<?php  namespace SynergyWholesale\Commands;
+
+use SynergyWholesale\Types\Domain;
+
+class DomainInfoCommandTest extends \PHPUnit_Framework_TestCase
+{
+	public function testCommand()
+	{
+		$command = new DomainInfoCommand(new Domain('example.com'));
+		$build = $command->getRequestData();
+
+		$this->assertTrue(is_array($build));
+		$this->assertArrayHasKey('domainName', $build);
+		$this->assertEquals('example.com', $build['domainName']);
+	}
+}
+
+?>
+ 
