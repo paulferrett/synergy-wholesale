085d381fd7c4c81fa1fdc41dc3eb6811250230b9
Simon Hampel <simon@hampelgroup.com>
2014-08-26 09:44:05 +1000

    added DomainRegisterUkResponse

---
+++ src/Responses/DomainRegisterUkResponse.php
@@ -0,0 +1,24 @@
+<?php  namespace SynergyWholesale\Responses;
+
+use SynergyWholesale\Exception\BadDataException;
+
+class DomainRegisterUkResponse extends Response
+{
+	protected $expectedFields = array('costPrice');
+
+	protected function validateData()
+	{
+		if (!is_numeric($this->response->costPrice))
+		{
+			throw new BadDataException("Expected a numeric cost price");
+		}
+	}
+
+	public function getCostPrice()
+	{
+		return $this->response->costPrice;
+	}
+}
+
+?>
+ 
---
+++ tests/Responses/DomainRegisterUkResponseTest.php
@@ -0,0 +1,40 @@
+<?php  namespace SynergyWholesale\Responses;
+
+use stdClass;
+
+class DomainRegisterUkResponseTest extends \PHPUnit_Framework_TestCase
+{
+	public function testMissingCostPrice()
+	{
+		$data = new stdClass();
+		$data->status = "OK";
+
+		$this->setExpectedException('SynergyWholesale\Exception\BadDataException', 'Expected property [costPrice] missing from response data');
+
+		new DomainRegisterUkResponse($data, 'DomainRegisterUkCommand');
+	}
+
+	public function testBadCostPrice()
+	{
+		$data = new stdClass();
+		$data->status = "OK";
+		$data->costPrice = "foo";
+
+		$this->setExpectedException('SynergyWholesale\Exception\BadDataException', 'Expected a numeric cost price');
+
+		new DomainRegisterUkResponse($data, 'DomainRegisterUkCommand');
+	}
+
+	public function testResponse()
+	{
+		$data = new stdClass();
+		$data->status = "OK";
+		$data->costPrice = "10.00";
+
+		$response = new DomainRegisterUkResponse($data, 'DomainRegisterUkCommand');
+		$this->assertEquals('10.00', $response->getCostPrice());
+	}
+}
+
+?>
+ 
