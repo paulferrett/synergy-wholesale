6d30d16039ba17f719c3723813b1d3bffcbcc82f
Simon Hampel <simon@hampelgroup.com>
2014-08-25 16:19:57 +1000

    unit tests for BusinessCheckRegistrationCommand

---
+++ tests/Commands/BusinessCheckRegistrationCommandTest.php
@@ -0,0 +1,32 @@
+<?php  namespace SynergyWholesale\Commands;
+
+use SynergyWholesale\Types\AuState;
+
+class BusinessCheckRegistrationCommandTest extends \PHPUnit_Framework_TestCase
+{
+	public function testNoState()
+	{
+		$command = new BusinessCheckRegistrationCommand('foo');
+		$build = $command->getRequestData();
+
+		$this->assertTrue(is_array($build));
+		$this->assertArrayHasKey('registrationNumber', $build);
+		$this->assertEquals('foo', $build['registrationNumber']);
+		$this->assertArrayNotHasKey('registrationState', $build);
+	}
+
+	public function testState()
+	{
+		$command = new BusinessCheckRegistrationCommand('foo', new AuState('NSW'));
+		$build = $command->getRequestData();
+
+		$this->assertTrue(is_array($build));
+		$this->assertArrayHasKey('registrationNumber', $build);
+		$this->assertEquals('foo', $build['registrationNumber']);
+		$this->assertArrayHasKey('registrationState', $build);
+		$this->assertEquals('NSW', $build['registrationState']);
+	}
+}
+
+?>
+ 
