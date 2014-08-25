dfdbf71bfcc37eb2a47bc0600f0d2bc24be9256a
Simon Hampel <simon@hampelgroup.com>
2014-08-25 16:20:48 +1000

    unit tests for CheckDomainResponse

---
+++ tests/Responses/CheckDomainResponseTest.php
@@ -0,0 +1,29 @@
+<?php  namespace SynergyWholesale\Responses;
+
+use stdClass;
+
+class CheckDomainResponseTest extends \PHPUnit_Framework_TestCase
+{
+	public function testResponse()
+	{
+		$data = new stdClass();
+		$data->status = "AVAILABLE";
+
+		$response = new CheckDomainResponse($data, 'CheckDomainCommand');
+
+		$this->assertEquals(true, $response->isAvailable());
+	}
+
+	public function testResponse2()
+	{
+		$data = new stdClass();
+		$data->status = "UNAVAILABLE";
+
+		$response = new CheckDomainResponse($data, 'CheckDomainCommand');
+
+		$this->assertEquals(false, $response->isAvailable());
+	}
+}
+
+?>
+ 
