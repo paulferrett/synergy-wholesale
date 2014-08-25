bb4be5a68d5b432c41207cde7759126ec8c60b44
Simon Hampel <simon@hampelgroup.com>
2014-08-25 16:16:38 +1000

    updates for BalanceQueryResponse; added unit tests

--- src/Responses/BalanceQueryResponse.php
+++ src/Responses/BalanceQueryResponse.php
@@ -4,6 +4,14 @@ class BalanceQueryResponse extends Response
 {
 	protected $expectedFields = array('balance');
 
+	protected function validateData()
+	{
+		if (!is_numeric($this->response->balance))
+		{
+			throw new BadDataException("Expected a numeric account balance");
+		}
+	}
+
 	public function getBalance()
 	{
 		return $this->response->balance;
---
+++ tests/Responses/BalanceQueryResponseTest.php
@@ -0,0 +1,30 @@
+<?php  namespace SynergyWholesale\Responses;
+
+use stdClass;
+
+class BalanceQueryResponseTest extends \PHPUnit_Framework_TestCase
+{
+	public function testMissingBalance()
+	{
+		$data = new stdClass();
+		$data->status = "OK";
+
+		$this->setExpectedException('SynergyWholesale\Exception\BadDataException', 'Expected property [balance] missing from response data');
+
+		new BalanceQueryResponse($data, 'BalanceQueryCommand');
+	}
+
+	public function testResponse()
+	{
+		$data = new stdClass();
+		$data->status = "OK";
+		$data->balance = 100;
+
+		$response = new BalanceQueryResponse($data, 'BalanceQueryCommand');
+
+		$this->assertEquals(100, $response->getBalance());
+	}
+}
+
+?>
+ 
