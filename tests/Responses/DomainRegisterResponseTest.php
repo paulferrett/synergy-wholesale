b914e4aca38356450bd4490dd01e97dc7ee688b6
Simon Hampel <simon@hampelgroup.com>
2014-08-26 16:09:59 +1000

    Added DomainRegisterResponse

---
+++ tests/Responses/DomainRegisterResponseTest.php
@@ -0,0 +1,40 @@
+<?php  namespace SynergyWholesale\Responses;
+
+use stdClass;
+
+class DomainRegisterResponseTest extends \PHPUnit_Framework_TestCase
+{
+	public function testMissingCostPrice()
+	{
+		$data = new stdClass();
+		$data->status = "OK";
+
+		$this->setExpectedException('SynergyWholesale\Exception\BadDataException', 'Expected property [costPrice] missing from response data');
+
+		new DomainRegisterResponse($data, 'DomainRegisterCommand');
+	}
+
+	public function testBadCostPrice()
+	{
+		$data = new stdClass();
+		$data->status = "OK";
+		$data->costPrice = "foo";
+
+		$this->setExpectedException('SynergyWholesale\Exception\BadDataException', 'Expected a numeric cost price');
+
+		new DomainRegisterResponse($data, 'DomainRegisterCommand');
+	}
+
+	public function testResponse()
+	{
+		$data = new stdClass();
+		$data->status = "OK";
+		$data->costPrice = "10.00";
+
+		$response = new DomainRegisterResponse($data, 'DomainRegisterCommand');
+		$this->assertEquals('10.00', $response->getCostPrice());
+	}
+}
+
+?>
+ 
