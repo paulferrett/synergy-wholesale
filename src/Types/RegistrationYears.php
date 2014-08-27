7c9435bb950983b720e4f675827f3f63a2db2f30
Simon Hampel <simon@hampelgroup.com>
2014-08-27 12:49:40 +1000

    added type RegistrationYears

---
+++ src/Types/RegistrationYears.php
@@ -0,0 +1,36 @@
+<?php  namespace SynergyWholesale\Types; 
+
+use SynergyWholesale\Exception\InvalidArgumentException;
+
+class RegistrationYears
+{
+	private $years;
+
+	function __construct($years)
+	{
+		if (empty($years) OR !is_numeric($years) OR is_float($years) OR $years < 1 OR $years > 10)
+		{
+			throw new InvalidArgumentException("Years parameter is required and should be a positive integer value");
+		}
+
+		$this->years = intval($years);
+	}
+
+	public function getYears()
+	{
+		return $this->years;
+	}
+
+	public function __toString()
+	{
+		return strval($this->years);
+	}
+
+	public function equals(RegistrationYears $other)
+	{
+		return $this->years === $other->years;
+	}
+}
+
+?>
+ 
---
+++ tests/Types/RegistrationYearsTest.php
@@ -0,0 +1,51 @@
+<?php  namespace SynergyWholesale\Types;
+
+class RegistrationYearsTest extends \PHPUnit_Framework_TestCase
+{
+	public function testBadYears()
+	{
+		$this->setExpectedException('SynergyWholesale\Exception\InvalidArgumentException', 'Years parameter is required and should be a positive integer value');
+
+		new RegistrationYears(null);
+	}
+
+	public function testBadYears2()
+	{
+		$this->setExpectedException('SynergyWholesale\Exception\InvalidArgumentException', 'Years parameter is required and should be a positive integer value');
+
+		new RegistrationYears(-1);
+	}
+
+	public function testBadYears3()
+	{
+		$this->setExpectedException('SynergyWholesale\Exception\InvalidArgumentException', 'Years parameter is required and should be a positive integer value');
+
+		new RegistrationYears(0);
+	}
+
+	public function testBadYears4()
+	{
+		$this->setExpectedException('SynergyWholesale\Exception\InvalidArgumentException', 'Years parameter is required and should be a positive integer value');
+
+		new RegistrationYears(1.5);
+	}
+
+	public function testBadYears5()
+	{
+		$this->setExpectedException('SynergyWholesale\Exception\InvalidArgumentException', 'Years parameter is required and should be a positive integer value');
+
+		new RegistrationYears(11);
+	}
+
+	public function testYears()
+	{
+		$years = new RegistrationYears(2);
+
+		$this->assertEquals(2, $years->getYears());
+		$this->assertEquals("2", strval($years));
+		$this->assertTrue($years->equals(new RegistrationYears("2")));
+	}
+}
+
+?>
+ 
