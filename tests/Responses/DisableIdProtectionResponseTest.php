93c1d51f4c31627a7ed06996cc08927d206ac949
Simon Hampel <simon@hampelgroup.com>
2014-08-27 18:07:21 +1000

    implemented disableIDProtection

---
+++ tests/Responses/DisableIdProtectionResponseTest.php
@@ -0,0 +1,19 @@
+<?php  namespace SynergyWholesale\Responses;
+
+use stdClass;
+
+class DisableIdProtectionResponseTest extends \PHPUnit_Framework_TestCase
+{
+	public function testResponse()
+	{
+		$data = new stdClass;
+		$data->status = "OK";
+
+		$response = new DisableIdProtectionResponse($data, 'DisableIdProtectionCommand');
+
+		$this->assertTrue($response->disableSuccessful());
+	}
+}
+
+?>
+ 
