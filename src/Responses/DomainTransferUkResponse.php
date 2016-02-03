56dc4b616e67e6a4309df5183955a3476050ba6d
Paul Ferrett <paul@paulferrett.com>
2016-02-03 17:09:38 +0800

    Add missing response class DomainTransferUKResponse

---
+++ src/Responses/DomainTransferUkResponse.php
@@ -0,0 +1,27 @@
+<?php namespace SynergyWholesale\Responses;
+
+use SynergyWholesale\Exception\BadDataException;
+
+class DomainTransferUkResponse extends Response
+{
+    protected $expectedFields = array('statusCode', 'reason');
+
+    protected function validateData()
+    {
+        if (!is_numeric($this->response->statusCode))
+        {
+            throw new BadDataException("Expected a numeric status code");
+        }
+    }
+
+    public function getStatusCode()
+    {
+        return $this->response->statusCode;
+    }
+
+    public function getReason()
+    {
+        return $this->response->reason;
+    }
+
+}
